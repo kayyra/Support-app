8d8702e0de2a2b808aa8df349137a97d089c5f3f
kayyra <anarodriguez.p@hotmail.es>
2023-07-07 09:28:42 +0200

    footer added

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './global/header/header.component';
+import { FooterComponent } from './global/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    FooterComponent,
+   
   ],
   imports: [
     BrowserModule,
