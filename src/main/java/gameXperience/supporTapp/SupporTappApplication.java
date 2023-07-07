9b7bb303d8a7bc5d37a4689d59d660638528a074
kayyra <anarodriguez.p@hotmail.es>
2023-07-07 07:54:43 +0200

    first commit

---
+++ src/main/java/gameXperience/supporTapp/SupporTappApplication.java
@@ -0,0 +1,13 @@
+package gameXperience.supporTapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupporTappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupporTappApplication.class, args);
+	}
+
+}
